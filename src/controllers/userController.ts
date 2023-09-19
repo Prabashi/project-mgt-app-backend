e3ff46c580d417306a0da95c07e9e7c187d36d54
Prabashi <prabhashi.mm@gmail.com>
2023-09-19 16:44:42 +0800

    Update error handling and error messages

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,16 +1,17 @@
 import { Request, Response } from "express";
 import User from "../models/User";
+import { BadRequestError } from "../middleware/errorMiddleware";
+import asyncHandler from "express-async-handler";
 
-const getUser = async (req: Request, res: Response) => {
+const getUser = asyncHandler(async (req: Request, res: Response) => {
   const userId = req.user?._id;
-  console.log("userId" + userId);
   const user = await User.findById(userId, "name email");
 
   if (!user) {
-    res.status(400);
+    throw new BadRequestError("User not available");
   }
 
   res.status(200).json(user);
-};
+});
 
 export { getUser };
